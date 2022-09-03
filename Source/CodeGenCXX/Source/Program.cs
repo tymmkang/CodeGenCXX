5434206e3acc0cc04b6d1c06107f6cf1f7bd7f00
tymmkang <tymmkang@gmail.com>
2022-09-04 00:02:01 +0900

    헤더, 소스 읽어오기

--- Source/CodeGenCXX/Source/Program.cs
+++ Source/CodeGenCXX/Source/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace CodeGenCXX
+﻿using ClangSharp.Interop;
+using System.Reflection;
+
+namespace CodeGenCXX
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var exeAsm = Assembly.GetExecutingAssembly();
+            var exeAsmFileInfo = new FileInfo(exeAsm.Location);
+            var projDir = exeAsmFileInfo.Directory!.Parent!.Parent!.Parent!.Parent!;
+
+            var headerFilePath = Path.Combine(projDir.FullName, "CXXClass.h");
+            var sourceFilePath = Path.Combine(projDir.FullName, "CXXClass.cpp");
+
+            if (!File.Exists(headerFilePath) ||
+                !File.Exists(sourceFilePath))
+            {
+                Console.WriteLine("Not found C++ header and source.");
+                return -1;
+            }
+
+            var headerRawStr = File.ReadAllText(headerFilePath);
+            var sourceRawStr = File.ReadAllText(sourceFilePath);
+
+            return 0;
         }
     }
 }
