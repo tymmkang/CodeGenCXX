f701b2433893c5762490f69ed99d9b8705eb0341
tymmkang <tymmkang@gmail.com>
2022-09-03 21:33:56 +0900

    프로젝트 구조 추가

---
+++ Source/Sample/Source/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+#if defined(DEBUG)
+#define TEST_VALUE = 123
+#elif defined(RELEASE)
+#define TEST_VALUE = 456
+#endif
+
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
